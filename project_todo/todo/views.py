e5bb2076930fa1ed15caf1ca93c163ba522d03c3
hissokababy <ibraimovanzor@gmail.com>
2025-03-08 12:47:44 +0500

    pagination done

--- project_todo/todo/views.py
+++ project_todo/todo/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from todo.permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-
 
 from todo.serializers import TaskSerializer
 from todo.models import Task
 
 # Create your views here.
+
+class TaskListPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
 
 class TaskListView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
@@ -16,6 +22,7 @@ class TaskListView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated,]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'due_date']
+    pagination_class = TaskListPagination
 
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
